409832705cef39383a4b67a16bee8af2939319c7
Isaac <esac-io@tutanota.com>
2020-11-04 13:20:24 -0200

    Update header commentaries

--- dash-docs.el
+++ dash-docs.el
@@ -1,29 +1,40 @@
 ;;; dash-docs.el --- Offline documentation browser using Dash docsets.  -*- lexical-binding: t; -*-
+;;
 ;; Copyright (C) 2013-2014  Raimon Grau
 ;; Copyright (C) 2013-2014  Toni Reina
 ;; Copyright (C) 2020       esac
-
+;;
 ;; Author: Raimon Grau <raimonster@gmail.com>
 ;;         Toni Reina  <areina0@gmail.com>
 ;;         Bryan Gilbert <bryan@bryan.sh>
 ;;         esac <esac-io@tutanota.com>
 ;;
+;; Version: Alpha 0.0.3
+;; Keywords: dash, docs, documentation
+;; URL: https://github.com/esac-io/dash-docs
+;;
+;; This file is NOT part of GNU Emacs.
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
-;; A library that provides ways to interact with dash-docs.
+;; This library provides ways to interact with Dash docs, i.e,
+;; install docsets archives from the internet, search entries in the
+;; local docsets database, provide candidates for the *Completions*
+;; `minibuffer' and open documentation (usually html files) using
+;; the chosen browser (eww, firefox and others).
 ;;
 ;;; Code:
 
